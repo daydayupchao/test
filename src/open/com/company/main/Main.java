d1c43ddf8a5952b16b7585727f2936a103d79bcb
chrisLi <m18833368330@163.com>
2020-04-03 09:38:57 +0800

    init

--- src/open/com/company/main/Main.java
+++ src/open/com/company/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("这是一个测试项目！！");
+        System.out.println("1111");
 	// write your code here
     }
 }
